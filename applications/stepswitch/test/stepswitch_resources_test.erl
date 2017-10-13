f19819944a783f7d62043c5e12f8e1b1ebe54346
Clayton Finney <claytonfinney@berkeley.edu>
2017-10-13 15:52:00 -0700

    ugly but working tests for sip_invite_parameters

--- applications/stepswitch/test/stepswitch_resources_test.erl
+++ applications/stepswitch/test/stepswitch_resources_test.erl
@@ -26,10 +26,9 @@ invite_parameters_test_() ->
     [GatewayJObj|_] = kz_json:get_value(<<"gateways">>, ResourceJObj),
     Resource = stepswitch_resources:resource_from_jobj(ResourceJObj),
     Gateway = stepswitch_resources:gateway_from_jobj(GatewayJObj, Resource),
-   % example L = [<<"npid">>, <<"location=test@2600hz.com">>, <<"account_id=Me">>, <<"0288">>]
-
-    %GatewayJObj1 = kz_json:set_value([<<"invite_parameters">>, <<"static">>], [<<"test">>], GatewayJObj),
-    %Gateway1 = stepswitch_resources:gateway_from_jobj(GatewayJObj1, Resource),
+  
+    GatewayJObj1 = kz_json:set_value([<<"invite_parameters">>, <<"static">>], [<<"test">>], GatewayJObj),
+    _Gateway1 = stepswitch_resources:gateway_from_jobj(GatewayJObj1, Resource),
 
     Offnet1 = kz_json:set_value([<<"Requestor-Custom-Channel-Vars">>, <<"TNS-CIC">>], <<"cic=2002">>, Offnet),
     Offnet2 = kz_json:set_value([<<"Requestor-Custom-Channel-Vars">>, <<"Account-ID">>], <<"12345">>, Offnet),
@@ -39,33 +38,22 @@ invite_parameters_test_() ->
     GatewayJObj2 = kz_json:set_value([<<"invite_parameters">>, <<"dynamic">>], [DynamicParameters1], GatewayJObj),
     Gateway2 = stepswitch_resources:gateway_from_jobj(GatewayJObj2, Resource),
 
-    %DynamicParameters2 = kz_json:from_list([{<<"key">>, <<"custom_sip_headers.x_auth_ip">>}, {<<"tag">>, <<"somethingelse">>}, {<<"seperator">>, <<"&">>}]),
-    
+    DynamicParameters2 = kz_json:from_list([{<<"key">>, <<"custom_sip_headers.x_auth_ip">>}, {<<"tag">>, <<"somethingelse">>}, {<<"seperator">>, <<"&">>}]),
+    GatewayJObj3 = kz_json:set_value([<<"invite_parameters">>, <<"dynamic">>], [DynamicParameters2], GatewayJObj),
+    Gateway3 = stepswitch_resources:gateway_from_jobj(GatewayJObj3, Resource),    
+
     Result1 = stepswitch_resources:sip_invite_parameters(Gateway, Offnet1),
     Result2 = stepswitch_resources:sip_invite_parameters(Gateway, Offnet2),
-    Result3 = stepswitch_resources:sip_invite_parameters(Gateway, Offnet3),    
-    Result4 = stepswitch_resources:dynamic_sip_invite_parameters(Gateway2, Offnet),
-    ?debugFmt("~p~n", [Gateway]),
+    Result3 = stepswitch_resources:sip_invite_parameters(Gateway, Offnet3),
+    Result4 = stepswitch_resources:sip_invite_parameters(Gateway2, Offnet3),
+    Result5 = stepswitch_resources:sip_invite_parameters(Gateway3, Offnet3),     
     
     [?_assertEqual([<<"npid">>, <<"zone=local">>, <<"cic=2002">>], Result1)
     ,?_assertEqual([<<"npid">>, <<"zone=local">>, <<"account_id=12345">>], Result2)
     ,?_assertEqual([<<"npid">>, <<"zone=local">>, <<"source_ip=127.0.0.1">>], Result3)
-    ,?_assertEqual([], Result4)
+    ,?_assertEqual([<<"npid">>, <<"somethingelse=127.0.0.1">>], Result4)
+    ,?_assertEqual([<<"npid">>, <<"somethingelse&127.0.0.1">>], Result5)
     ]. 
-
-%    Setup = [{<<"cic=2002">>
-%             ,[<<"Requestor-Custom-Channel-Vars">>, <<"TNS-CIC">>]
-%             ,fun kz_json:get_value/2
-%             ,fun kz_json:set_value/3
-%             ,Offnet},
-%             {<<"12345">>
-%             ,[<<"Requestor-Custom-Channel-Vars">>, <<"Account-ID">>]
-%             ,fun kz_json:get_value/2
-%             ,fun kz_json:set_value/3
-%             ,Offnet}
-%            ],
-%    [?_assertEqual(Expected, Getter(Path, Setter(Path, Expected, Object))) 
-%               || {Expected, Path, Getter, Setter, Object} <- Setup].
 
 validate(Schema, Object) ->
     kz_json_schema:validate(Schema
